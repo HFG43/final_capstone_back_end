7dd735ef4b31b17d1b98ffbf8cf63fd4b0a0f02e
Carmen Bravo <nbravoalcala@gmail.com>
2023-10-27 19:19:11 -0400

    Create the show method in ExperiencesController to get data from one experience

--- app/controllers/api/v1/experiences_controller.rb
+++ app/controllers/api/v1/experiences_controller.rb
@@ -4,4 +4,12 @@ class Api::V1::ExperiencesController < ApplicationController
     experiences = Experience.all
     render json: experiences
   end
+
+  # GET /experiences/:id
+  def show
+    experience = Experience.find(params[:id])
+    render json: experience
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: 'Record not found' }, status: 500
+  end
 end
