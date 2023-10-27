863e36f5a6f716cb2d6676db4365be11a5ede078
Andrea Manuel <andreamanuel2207@gmail.com>
2023-10-27 17:16:44 -0600

    Fix rubocop linters

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,6 +1,7 @@
 class Api::UsersController < ApplicationController
   def show
-    unless user = User.find_by(username: params[:username])
+    user = User.find_by(username: params[:username])
+    if user.nil?
       render json: { exist: false }
     else
       render json: { exist: true }
@@ -8,17 +9,17 @@ class Api::UsersController < ApplicationController
   end
 
   def create
-      json_request = JSON.parse(request.body.read)
+    json_request = JSON.parse(request.body.read)
 
-      name = json_request['name']
-      username = json_request['username']
+    name = json_request['name']
+    username = json_request['username']
 
-      new_user = User.new(name:, username:)
+    new_user = User.new(name:, username:)
 
-      if new_user.save
-        render json: new_user, status: :created
-      else
-        render json: { errors: new_user.errors.full_messages }, status: :unprocessable_entity
-      end
+    if new_user.save
+      render json: new_user, status: :created
+    else
+      render json: { errors: new_user.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 end
