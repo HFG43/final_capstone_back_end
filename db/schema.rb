f10a260113c74a9c7986a0a32aa07a4c553eac7e
Carmen Bravo <nbravoalcala@gmail.com>
2023-10-30 16:25:06 -0400

    Add the image field to the experiences table and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_27_213236) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_30_195316) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_27_213236) do
     t.integer "guests", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image", null: false
   end
 
   create_table "reservations", force: :cascade do |t|
