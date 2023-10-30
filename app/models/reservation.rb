dd0c2ae8934f82c32385302f7edfd09fd84499d9
Dachrono <andres_zamorano_@hotmail.com>
2023-10-30 09:40:46 -0600

    Add relations for user and experience, add some primary test for reservations

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,2 +1,4 @@
 class Reservation < ApplicationRecord
+    belong_to :user, class_name 'User', foreign_key: 'users_id'
+    belong_to :experience, class_name 'Experience', foreign_key: 'experiences_id'
 end
