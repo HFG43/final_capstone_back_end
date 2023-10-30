4a9a20964f449d84b819bf86fdc037d78581da3a
Dachrono <andres_zamorano_@hotmail.com>
2023-10-30 10:08:04 -0600

    Solve some rubocop issues

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,7 +1,7 @@
 class ReservationsController < ApplicationController
   def index
     reservations = Reservation.all
-    render json: reservations  
+    render json: reservations
   end
 
   def delete
