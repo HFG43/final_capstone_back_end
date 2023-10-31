312b8c2fec9eb398d082c86344a382684df9172e
Carmen Bravo <nbravoalcala@gmail.com>
2023-10-31 17:38:08 -0400

    Update the attributes validations in the Experience model

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -5,6 +5,6 @@ class Experience < ApplicationRecord
   # Validations.
   validates :name, :description, :image, :experience_fee, :add_testing_fee, :reserve_full_table, :guests, presence: true
   validates :name, length: { maximum: 250 }
-  validates :experience_fee, :add_testing_fee, :reserve_full_table, comparison: { greater_than_or_equal_to: 0 }
-  validates :guests, comparison: { greater_than_or_equal_to: 0 }, numericality: { only_integer: true }
+  validates :experience_fee, :add_testing_fee, :reserve_full_table, comparison: { greater_than: 0 }
+  validates :guests, comparison: { greater_than: 0 }, numericality: { only_integer: true }
 end
